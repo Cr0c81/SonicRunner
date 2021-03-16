78d87f2663d59b44fc62a54d736abdee19a32afb
Cr0c <sms0.mail.ru@gmail.com>
2021-03-16 17:47:34 +0300

    wip

--- Assets/Scripts/Player/GameplayController.cs
+++ Assets/Scripts/Player/GameplayController.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Bonus;
 using Dreamteck.Splines;
@@ -17,22 +19,29 @@ namespace Player
 		[SerializeField] private float _speedMultNeeded = 1f;
 		[SerializeField] private float _speedMultSpeed  = 1f;
 		[SerializeField] private float _speedMultTimer  = 0f;
+		[SerializeField] private float _jumpHeight      = 4f;
 
 		private                  Transform        _playerTransform;
+		private                  Vector3          _playerOffset = new Vector3(0f, 0f, 0f);
 		private                  Animator         _playerAnimator;
 		private                  SplineComputer[] _tracks;
 		private                  float            _track            = 1;
 		private                  int              _trackNeeded      = 1;
 		[SerializeField] private float            _trackChangeSpeed = 1f;
 
-		private                 IScoreWidget _scoreWidget;
-		private                 int          _score;
-		private                 double[]     _projects;
-		private                 bool         _isReady = false;
-		private                 int          _startTrack;
-		private static readonly int          Jump      = Animator.StringToHash("jump");
-		private static readonly int          Roll      = Animator.StringToHash("roll");
-		private static readonly int          MoveSpeed = Animator.StringToHash("moveSpeed");
+		private IScoreWidget _scoreWidget;
+		private int          _score;
+		private double[]     _projects;
+		private bool         _isReady = false;
+		private int          _startTrack;
+
+		private                 float _animLegJumpB;
+		private                 float _animLegRollkickA;
+		private static readonly int   Jump      = Animator.StringToHash("jump");
+		private static readonly int   Roll      = Animator.StringToHash("roll");
+		private static readonly int   MoveSpeed = Animator.StringToHash("moveSpeed");
+		private static readonly int   JumpSpeed = Animator.StringToHash("jumpSpeed");
+		private static readonly int   RollSpeed = Animator.StringToHash("rollSpeed");
 
 		private void Awake()     => Register();
 		private void OnDestroy() => Unregister();
@@ -41,7 +50,7 @@ namespace Player
 			if (!_isReady) return;
 			_track = Mathf.MoveTowards(_track, _trackNeeded, _trackChangeSpeed * Time.deltaTime);
 			for (var i = 0; i < _tracks.Length; i++)
-				_projects[i] = _tracks[i].Project(_playerTransform.position);
+				_projects[i] = _tracks[i].Project(_playerTransform.position - _playerOffset);
 			var track1 = Mathf.FloorToInt(_track);
 			var track2 = track1 + 1;
 			if (track2 >= _tracks.Length) track2--;
@@ -49,7 +58,7 @@ namespace Player
 			var t2  = _tracks[track2].Evaluate(_projects[track2]);
 			var pos = Vector3.Lerp(t1.position, t2.position, _track     % 1);
 			var rot = Quaternion.Slerp(t1.rotation, t2.rotation, _track % 1);
-			_playerTransform.position = pos;
+			_playerTransform.position = pos + _playerOffset;
 			_playerTransform.rotation = rot;
 
 			if (_movePlayer)
@@ -109,16 +118,17 @@ namespace Player
 		public void StartGame(SplineComputer[] tracks, Transform player, int track)
 		{
 			var input = Locator.GetObject<InputController>();
-			input.onInput             += OnInput;
-			_tracks                   =  tracks;
-			_startTrack               =  track;
-			_trackNeeded              =  _startTrack;
-			_track                    =  _startTrack;
-			_projects                 =  new double[_tracks.Length];
-			_playerTransform          =  player;
-			_playerAnimator           =  _playerTransform.GetComponentInChildren<Animator>();
-			_playerTransform.position =  _tracks[(int)_track].Evaluate(0).position;
-			_playerTransform.rotation =  _tracks[(int)_track].Evaluate(0).rotation;
+			input.onInput    += OnInput;
+			_tracks          =  tracks;
+			_startTrack      =  track;
+			_trackNeeded     =  _startTrack;
+			_track           =  _startTrack;
+			_projects        =  new double[_tracks.Length];
+			_playerTransform =  player;
+			_playerAnimator  =  _playerTransform.GetComponentInChildren<Animator>();
+			GetAnimatorClips();
+			_playerTransform.position = _tracks[(int)_track].Evaluate(0).position;
+			_playerTransform.rotation = _tracks[(int)_track].Evaluate(0).rotation;
 
 			player.GetComponentInChildren<ColliderProxy>().onTrigger += OnPlayerTrigger;
 
@@ -131,6 +141,15 @@ namespace Player
 			_scoreWidget.Show();
 			_isReady    = true;
 			_movePlayer = true;
+		}
+
+		private void GetAnimatorClips()
+		{
+			var aoc   = (AnimatorOverrideController)_playerAnimator.runtimeAnimatorController;
+			var clips = new List<KeyValuePair<AnimationClip, AnimationClip>>();
+			aoc.GetOverrides(clips);
+			_animLegJumpB     = clips.Select(t => t.Key).FirstOrDefault(t => t.name.Equals("Anim_Leg_JumpB")).length     / _playerAnimator.GetFloat(JumpSpeed);
+			_animLegRollkickA = clips.Select(t => t.Key).FirstOrDefault(t => t.name.Equals("Anim_Leg_RollkickA")).length / _playerAnimator.GetFloat(RollSpeed);
 		}
 		private void FinishGame()
 		{
@@ -158,6 +177,7 @@ namespace Player
 					break;
 				case InputController.InputType.Jump:
 					_playerAnimator.SetTrigger(Jump);
+					this.InvokeDelegate((value) => { _playerOffset = new Vector3(0f, Mathf.PingPong(value * 2f, 1f) * _jumpHeight, 0f); }, _animLegJumpB);
 					break;
 				case InputController.InputType.Roll:
 					_playerAnimator.SetTrigger(Roll);
